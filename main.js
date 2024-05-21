2939e31c1dbd733fb0cf3fb0dea1568c5149dee2
Prof-Gui <guilhermeguanais@prof.educacao.sp.gov.br>
2024-05-21 16:02:42 -0300

    tentativa de correção de erro

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ let nomeUsuário = "";
 let elemento = document.querySelector("#nome-usuário");
 
 while(nomeUsuário == ""){
-    nomeUsuário = prompt("Qual o seu nome?")
+    nomeUsuário = prompt("Qual o seu nome?");
 }
 
 if(nomeUsuário == null){
-    elemento.textContent = "Senhor(a)";
+    elemento.textContent = 'senhor(a)';
 }else{
     elemento.textContent = nomeUsuário;
 }
